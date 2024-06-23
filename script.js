629d4c70d9b9735f347c2865ebf1959daa425624
DanielNoronhaOliveira <daniel.novaorden33@gmail.com>
2024-06-23 19:42:55 -0300

    finalizado

---
+++ script.js
@@ -0,0 +1,9 @@
+function fale(){
+   document.querySelector("form").style.opacity=1
+   document.querySelector(".caixa-maior").style.opacity=0.3
+    
+}
+function voltar(){
+    document.querySelector("form").style.opacity=0
+    document.querySelector(".caixa-maior").style.opacity=1
+}
